8b4e78c47ee4d61d9085fa5e4a354c1bc1964ab1
barkern89 <barkern89@gmail.com>
2019-10-07 20:22:12 -0700

    FINALLY finished callbacks.js not including stretch goal

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,20 +1,18 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-
-const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
-
-/* 
+// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
+ 
 
   // GIVEN THIS PROBLEM:
-
+/*
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
 
-  // SOLUTION:
+      // SOLUTION:
 
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+      function firstItem(arr, cb) {
+        return cb(arr[0]);
+      }
 
   // NOTES ON THE SOLUTION:
 
@@ -25,7 +23,7 @@ const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
   // TEST 1 (inlined callback):
 
-  const test1 = firstItem(items, item => `I love my ${item}!`);
+  const test1 = function firstItem(items, item => `I love my ${item}!`);
   console.log(test1); // "I love my Pencil!"
 
   // TEST 2 (declaring callback before hand):
@@ -38,28 +36,58 @@ const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
+
+//Higher order function for getLength and last functions:
+function itemsHigherOrderFunction(items,cb){
+  return cb(items);
 }
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    function getLength(items) {
+      return `There are ${items.length} items in the array`;
+      // getLength passes the length of the array into the callback.
+    }
+    console.log(itemsHigherOrderFunction(items,getLength));
+
+
+
+    function last(items) {
+      // last passes the last item of the array into the callback.
+      return `The last item in the array is ${items[3]}`;
+    }
+    console.log(itemsHigherOrderFunction(items,last))
+
+//Higher order function for sumNums and multiplyNums:
+
+function doMath(x,y,callback){
+  return callback(x,y);
 }
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+    function sumNums(x,y) {
+      // sumNums adds two numbers (x, y) and passes the result to the callback.
+      return x + y;
+    }
+    console.log(doMath(5,5,sumNums));
 
-function contains(item, list, cb) {
+    function multiplyNums(x, y) {
+      // multiplyNums multiplies two numbers and passes the result to the callback.
+      return x * y;
+    }
+
+    console.log(doMath(5,5,multiplyNums))
+
+
+
+function contains(item, list, callback) {
+    callback(list.includes(item));
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-
+return (contains('Pencil',items,console.log))
 /* STRETCH PROBLEM */
+
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
